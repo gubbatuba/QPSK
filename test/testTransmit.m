70a44c9888b6ec8c02f046cc2ff42ced7080aacb
Jennifer Wei <jennifer.wei@students.olin.edu>
2017-04-24 16:10:58 -0400

    UPDATE: testTransmit code with updated parameters used in Office Hours

--- test/testTransmit.m
+++ test/testTransmit.m
@@ -1,11 +1,17 @@
-% address='30C628D';
-% tx = comm.SDRuTransmitter('Platform','B210','SerialNum',address,'Gain', 200,'CenterFrequency',2.5e9)
-% tx = comm.SDRuTransmitter('Platform','B210','SerialNum',address,'Gain', 200,'CenterFrequency',2.5e9,'InterpolationFactor',128,'MasterClockRate',2*32000000);
+%% OLD
+% tx = comm.SDRuTransmitter('Platform','B210','SerialNum',address,'Gain',
+% 200,'CenterFrequency',2.5e9) % default InterpolationFactor = 512
+% tx = comm.SDRuTransmitter('Platform','B210','SerialNum',address,'Gain', 200,'CenterFrequency',2.5e9,'EnableBurstMode',true,'NumFramesInBurst',2655,'InterpolationFactor',256,'MasterClockRate',2*32000000);
 
-dat = sign(randn(1e3,1));
-dat_us = upsample(dat, 100);
-p = 0.5*ones(100,1);
-tmp = conv(dat_us, p);
-x = zeros(1e3+length(tmp),1);
-x(1e3+1:end)  = tmp;
-step(tx,x);
+%% RUN THIS ON START ONCE THEN COMMENT OUT
+address='30C628D';
+tx = comm.SDRuTransmitter('Platform','B210','SerialNum',address,'Gain', 200,'CenterFrequency',2.5e9,'InterpolationFactor',256,'MasterClockRate',1.5*32000000);
+
+%% RUN THIS TO TRANSMIT
+% dat = sign(randn(1e2,1));
+% dat_us = upsample(dat, 100);
+% p = 0.5*ones(100,1);
+% tmp = conv(dat_us, p);
+% x = zeros(1e3+length(tmp),1);
+% x(1e3+1:end)  = tmp;
+% step(tx,x);
