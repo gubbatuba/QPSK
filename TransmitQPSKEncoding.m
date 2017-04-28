721c6f91f50d9662ed03ed5baf86f09524172174
mjakus <margaret.jakus@students.olin.edu>
2017-04-28 10:41:24 -0400

    Update TransmitQPSKEncoding.m
    Made it cleaner with Siddhartan's suggestions

--- TransmitQPSKEncoding.m
+++ TransmitQPSKEncoding.m
@@ -17,31 +17,18 @@ wordstring = 'beees';
     totalmatrix = I + Q;
     totalresult = flipud(rot90(totalmatrix));
 
+    [rowtotal,coltotal] = size(totalresult)
+    
+    data = reshape(totalmatrix, rowtotal*coltotal,1);
+    
 %     address='30C628D';
-%     tx = comm.SDRuTransmitter('Platform','B210','SerialNum',address,'Gain', 200,'CenterFrequency',2.5e9,'InterpolationFactor',256,'MasterClockRate',1.5*32000000);
+%    tx = comm.SDRuTransmitter('Platform','B210','SerialNum',address,'Gain', 200,'CenterFrequency',2.5e9,'InterpolationFactor',256,'MasterClockRate',1.5*32000000);
 
     p = 0.5*ones(100,1);
-    [r,c] = size(totalresult);
-% %     
-%     for i = 1:20
 
-        for col = 1:c
-            data = (totalresult(:,c));
-            data2 = repmat(data,100,1); % lengthens matrix
-            dat_us = upsample(data2,100); % to prevent switching too fast    
-            tmp = conv(tmp,p); % to make an actual square wave
-            x = zeros(1e3+length(tmp),1);
-            x(1e3+1:end)=tmp;
-            step(tx,x);
-        end
-        
-%     end
-    
-% dat = sign(randn(1e2,1));
-% dat_us = upsample(dat, 100);
-% p = 0.5*ones(100,1);
-% tmp = conv(dat_us, p);
-% x = zeros(1e3+length(tmp),1);
-% x(1e3+1:end)  = tmp;
-% step(tx,x);
-
+    data2 = repmat(data,10,1); % lengthens matrix
+    dat_us = upsample(data2,100); % to prevent switching too fast    
+    tmp = conv(dat_us,p); % to make an actual square wave
+    x = zeros(1e3+length(tmp),1);
+    x(1e3+1:end)=tmp;
+    step(tx,x);
